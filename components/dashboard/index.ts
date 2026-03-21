c7f2f873005676daf6ca7551360dfa23e9edde60
development <dev@vmi3057095.contaboserver.net>
2026-03-21 21:33:30 +0100

    feat(dashboard): add dashboard components index file

---
+++ components/dashboard/index.ts
@@ -0,0 +1,4 @@
+export { DashboardGreeting } from "./greeting";
+export { QuickStats, type BusinessStats, type WorkerStats } from "./quick-stats";
+export { UpcomingBookings } from "./upcoming-bookings";
+export { QuickActions } from "./quick-actions";
